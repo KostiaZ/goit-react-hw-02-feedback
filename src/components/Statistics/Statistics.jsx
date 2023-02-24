6ecba6d9908966b6eed80c364c7a4ceccd30f3df
Kostiantyn Zabolotnii <zabolotnii.kostiantyn@gmail.com>
2023-02-24 21:15:41 +0200

    fix percent at statistic

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -9,7 +9,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
         <li>neutral: {neutral}</li>
         <li>bad: {bad}</li>
         <li>total: {total}</li>
-        <li>positive: {positivePercentage}</li>
+        <li>positive: {positivePercentage}%</li>
       </ul>
     </div>
   );
